6aa49da05349270d1d68afa1c74e67f07db7308f
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-06-13 17:25:26 -0400

    Initial Commit for Social Geeking

---
+++ src/main/java/com/goruslan/socialgeeking/SocialgeekingApplication.java
@@ -0,0 +1,14 @@
+package com.goruslan.socialgeeking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialgeekingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialgeekingApplication.class, args);
+    }
+
+}
+
