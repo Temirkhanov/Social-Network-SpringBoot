e7fe5010b08de80261d948ef31a10f704e934ecd
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-06-26 15:19:49 -0400

    Profile and Register Templates and Controller

--- src/main/java/com/goruslan/socialgeeking/SocialgeekingApplication.java
+++ src/main/java/com/goruslan/socialgeeking/SocialgeekingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class SocialgeekingApplication {
@@ -20,5 +21,11 @@ public class SocialgeekingApplication {
         return new PrettyTime();
     }
 
+    // TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+    // TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
 
 }
--- src/main/java/com/goruslan/socialgeeking/controller/AuthController.java
+++ src/main/java/com/goruslan/socialgeeking/controller/AuthController.java
@@ -12,4 +12,16 @@ public class AuthController {
     }
 
 
+    @GetMapping("/profile")
+    public String profile() {
+        return "auth/profile";
+    }
+
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
+
+
 }
