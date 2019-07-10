8c2b5baf3f14929d8869fe02a3ba9f30a92df07b
Ruslan Temirkhanov <rusl0424@gmail.com>
2019-07-10 00:05:37 -0400

    Added Transactional saveUsers method to UserService

--- src/main/java/com/goruslan/socialgeeking/service/UserService.java
+++ src/main/java/com/goruslan/socialgeeking/service/UserService.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -22,6 +25,15 @@ public class UserService {
 
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+
+    @Transactional
+    public void saveUsers(User... users) {
+        for(User user : users) {
+            logger.info("Saving User: " + user.getEmail());
+            userRepository.save(user);
+        }
     }
 
 }
